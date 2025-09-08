174f83870c69e94ee72ae033c1361d7a163e6de6
Bendoke119 <fatyolbando1@gmail.com>
2025-09-08 14:03:44 +0200

    Add files via upload

---
+++ fela1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fela1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Téglalap kerülete és területe");
+            Console.Write("Kérem az a oldalt");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Kérem a b oldlat");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("A kerület: "+ 2 * a + 2 * b);
+            double ker = 2 * a + 2 * b;
+            double ter = a * b;
+            Console.WriteLine($"A terület {ter}");
+            if (ker == ter)
+            {
+                Console.WriteLine("Egyenlő");
+            }
+            else if (ker > ter)
+            {
+                Console.WriteLine("A kerület nagyobb");
+            }
+            else
+            {
+                Console.WriteLine("A terület nagyobb");
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Tanár diák kocka játék");
+            int tanar=rnd.Next(1,6);
+            Console.WriteLine($"A tanár {tanar} dobott");
+            int diak = rnd.Next(1, 6);
+            Console.WriteLine($"A diák {diak} dobott");
+            if (tanar > diak)
+            {
+                Console.WriteLine("A tanár nyert");
+
+            }
+            else if (tanar < diak)
+            {
+                Console.WriteLine("A diák nyert");
+
+            }
+            else
+            {
+                Console.WriteLine("hát");*/
+            for (int i = 1;i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Hajrá zete!!!!");
+            }
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("Fújpest!!!!");
+            }
+        }
+        }
+        
+    }
+
