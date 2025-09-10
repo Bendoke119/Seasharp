4047d243f1f64b6b4343b561b86323f40e27e309
Bendoke119 <fatyolbando1@gmail.com>
2025-09-10 12:24:02 +0200

    Add files via upload

--- fela1.cs
+++ fela1.cs
@@ -50,7 +50,7 @@ namespace fela1
             }
             else
             {
-                Console.WriteLine("hát");*/
+                Console.WriteLine("hát");
             for (int i = 1;i <= 5; i++)
             {
                 Console.WriteLine(i);
@@ -63,8 +63,105 @@ namespace fela1
             {
                 Console.WriteLine("Fújpest!!!!");
             }
+            Console.Write("Kérem az a oldalt");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Kérem a b oldalt");
+            double b = double.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("Az A oldal nagyobb mint a B");
+            }
+            if (a < b)
+            {
+                Console.WriteLine("A B oldal nagyobb mint az A");
+            }
+           
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Hajrá zete!!!!");
+                for (int i2 = 1; i2 <= 3; i2++)
+                {
+                    Console.WriteLine("Fújpest!!!!");
+                }
+            }
+            string[] nevek = { "Anna", "Béla", "Csaba" };
+
+            for (int i = 0; i < nevek.Length; i++)
+            {
+                Console.WriteLine(nevek[i]);
+            }
+            string[] nevek = { "Anna", "Béla", "Csaba" };
+            foreach (string nev in nevek)
+            {
+                Console.WriteLine(nev);
+            }
+            string[] cuccok = { "  |", "  |", "  V", "A legkomolyabb wingman" };
+            foreach (string cnev in cuccok)
+            {
+                Console.WriteLine(cnev);
+
+            }
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i < 5);
+            int[] szamok = { 12, 5, 8, -3 };
+            int min = szamok[0];
+            for (int i = 1; i < szamok.Length; i++)
+            {
+                if (szamok[i] < min)
+                {
+                    min = szamok[i];
+                }
+            }
+            Console.WriteLine("A legkissebb szám: " +min);
+
+            int[] szamok = { 12, 5, 8, -3 };
+            int min = szamok[0];
+            foreach (int szam in szamok)
+            { 
+                if (szam < min)
+                {
+                    min = szam;
+                }
+            }
+            Console.WriteLine(min);
+            int[] szamok = { 12, 5, 8, -3 };
+            int min = szamok[0];
+            int i = 1;
+            while (i < szamok.Length)
+            {
+                if (szamok[i] < min)
+                {
+                    min = szamok[i];
+                }
+                i++;
+            }
+            Console.WriteLine("A legkissebb szám: " + min);*/
+
+            int[] szamok = { 12, 5, 8, -3 };
+            int min = szamok[0];
+            int i = 1;
+            do
+            {
+                if (szamok[i] < min)
+                {
+                    min = szamok[i];
+                }
+                i++;
+            }while (i < szamok.Length);
+            Console.WriteLine("A legkissebb szám: " + min);
+
+
         }
-        }
-        
     }
+}
 
