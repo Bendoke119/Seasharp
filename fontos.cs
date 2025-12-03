866162be22b820f6662e8bd600ea20b5d349b8f2
Bendoke119 <fatyolbando1@gmail.com>
2025-12-03 12:05:16 +0100

    Add files via upload

--- fontos.cs
+++ fontos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -61,15 +62,93 @@ namespace fela12
             for (int i = 1; i <= 5; i++) //Olyan mint a while a py-ban
             {
                 Console.WriteLine(i);
-            }*/
+            }
             for(int i = 1; i <= 5; i++)
             {
                 Console.WriteLine("Hajrá ZTE");
             }
-            for (int i = 1; i <= 3; i++)
+            for (int i = 1; i <= 3; i++) //Olyan mint a while ciklus (változó, művelet ,hozzá adunk eggyet)
             {
                 Console.WriteLine("Fujpest");
             }
+            
+            string[] nevek = { "Zebulon", "Béci", "Boda" }; //string változó mert szavakat(neveket) tárolunk benne
+
+            for (int i = 0; i < nevek.Length; i++)
+            {
+                Console.WriteLine(nevek[i]); //Az i változó a nevek kiírásáért felel ezért szükséges
+            }
+            string[] nevek = { "Zebulon", "Béci", "Boda" };
+
+            foreach (string nev in nevek) //Olyan mint a jól ismert for ciklus a python-ból + a "string" előjel itt is és máshol is fontos! 
+            {
+                Console.WriteLine(nev);
+            }
+            int i = 0;
+            while (i < 5) //A while ciklus ugyanolyan mint a python-os változata
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i < 5); //Ugyanazt csinálja mint az előző while-os csak itt a feltételt az új "do" művelet után írjuk.
+            
+            int[] szamok = { 12, 8, 5, 3, 1 };
+            int min = szamok[0];
+            for (int i = 1; i < szamok.Length; i++)
+            {
+                if (szamok[i] < min)
+                {
+                    min = szamok[i];
+                }
+            }
+            Console.WriteLine(min);
+
+            int[] szamok = { 12, 8, 5, 3, 1 };
+            int min = szamok[0];
+
+            foreach (int szam in szamok)
+            {
+                if (szam < min)
+                {
+                    min = szam;
+                }
+            }
+            int[] szamok = { 12, 8, 5, 3, 1 };
+            int min = szamok[0];
+            int i = 1;
+            while (i < szamok.Length)
+            {
+                if (szamok[i] < min)
+                {
+                    min = szamok[i];
+                }
+                i++;
+            }
+            Console.WriteLine("A legkisebb szám: " + min);
+
+            int[] szamok = { 12, 8, 5, 3, 1 , -56 };
+            int min = szamok[0]; //A minimumnak nem muszály az pont elsö elemmel egyenlőnek lennie hanem más elemmel is működik
+            int i = 1;
+            do
+            {
+                if (szamok[i] < min)
+                {
+                    min = szamok[i];
+                }
+                i++;
+            } while (i < szamok.Length);
+            Console.WriteLine("A legkisebb szám: " + min);*/
+
+            int[] szamok = { 12, 8, 5, 3, 1, -56 };
+
+            int min = szamok.Min();
+            Console.WriteLine($"A legkisebb szám: {min}"); 
 
         }
     }
