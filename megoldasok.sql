9f6d66a9a17b6f4768fa3795c992818c7e580387
Bendoke119 <fatyolbando1@gmail.com>
2026-01-28 10:35:04 +0100

    Add files via upload

---
+++ megoldasok.sql
@@ -0,0 +1,31 @@
+﻿-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+-- 9. feladat:
+CREATE DATABASE vedettmadarak
+DEFAULT CHARACTER SET='UTF8'
+COLLATE utf8_hungarian_ci
+
+-- 11. feladat:
+
+UPDATE faj
+SET faj.ertek = 25
+WHERE faj.id = 352
+
+-- 12. feladat: 
+SELECT faj.nev, faj.latin
+FROM faj    
+ORDER BY faj.nev;
+
+-- 13. feladat:
+SELECT faj.nev, faj.evszam, COUNT(faj.id) AS db
+FROM faj
+WHERE faj.evszam > 2000
+GROUP BY faj.evszam
+ORDER BY faj.evszam
+
+
+-- 14. feladat:
+    
+ 
+-- 15. feladat:
+
