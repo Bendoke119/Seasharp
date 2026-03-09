91e7db9aa76aeceb7776654762f231b0007b42d6
Bendoke119 <fatyolbando1@gmail.com>
2026-03-09 13:52:36 +0100

    Add files via upload

---
+++ fela7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fela5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mappaUtvonal = @"C:\Users\info\Desktop\Bendőmendő\okosssag";
+            string fajlNev = "elso.txt";
+            string teljesUtvonal = Path.Combine(mappaUtvonal, fajlNev);
+
+            Directory.CreateDirectory(mappaUtvonal);
+
+            Console.WriteLine("Írj be sorokat (vége: 'vege'):");
+            using (StreamWriter sw = new StreamWriter(teljesUtvonal))
+            {
+                string sor;
+                while ((sor = Console.ReadLine()) != "vege")
+{
+                    sw.WriteLine(sor);
+                }
+            }
+        }
+    }
+}
